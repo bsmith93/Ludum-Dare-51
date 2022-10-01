af92efc7a97968ae9a8b0a33b0f792fa8fa880bb
tom_miracletea <tom@miracleteastudios.com>
2022-10-01 05:24:14 +0200

    - Latest

---
+++ Ludum Dare 51/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public LevelBuilder m_LevelBuilder;
+	public GameObject m_NextButton;
+	private bool m_ReadyForInput;
+	public Player m_Player;
+
+	private void Start()
+	{
+		m_LevelBuilder.Build();
+		m_Player = FindObjectOfType<Player>();
+	}
+
+	private void Update()
+	{
+		Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		moveInput.Normalize();
+		if(moveInput.sqrMagnitude > 0.5)
+		{
+			if (m_ReadyForInput)
+			{
+				m_ReadyForInput = false;
+				m_Player.Move(moveInput);
+			}
+		}
+		else
+		{
+			m_ReadyForInput = true;
+		}
+	}
+}
